44c9d10608f9e78cc47a73e25b8afa3cb1e62ec5
Lev Maximov <lev.maximov@gmail.com>
2023-12-18 16:00:53 +0700

    telegram bot

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from flask import Flask
+from flask import Flask, request
+import os
+import requests
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!!!'
+def send_message(chat_id, text):
+    method = "sendMessage"
+    token = os.environ['TELEGRAM_TOKEN']
+    url = f"https://api.telegram.org/bot{token}/{method}"
+    data = {"chat_id": chat_id, "text": text}
+    requests.post(url, data=data)
+
+@app.route("/", methods=["GET", "POST"])
+def receive_update():
+    if request.method == "POST":
+        print(request.json)
+        chat_id = request.json["message"]["chat"]["id"]
+        send_message(chat_id, "pong")
+    return {"ok": True}
