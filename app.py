18d14a3f0bc019be9e5291fa00ec2c331fae4913
Александр Коробов <aleksandrkorobov@MacBook-Air-Aleksandr.local>
2023-12-20 06:30:11 +0700

    update add.py

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
 from flask import Flask, request
-import os
 import requests
+import os
 
 app = Flask(__name__)
 
-def send_message(chat_id, text):
-    method = "sendMessage"
+def send_message(chat_id, message_id, text):
+    method = 'sendMessage'
     token = os.environ['TELEGRAM_TOKEN']
-    url = f"https://api.telegram.org/bot{token}/{method}"
-    data = {"chat_id": chat_id, "text": text}
+    url = f'https://api.telegram.org/bot{token}/{method}'
+    data = {'chat_id': chat_id, 'reply_to_message_id': message_id, 'text': text}
     requests.post(url, data=data)
-
-@app.route("/", methods=["GET", "POST"])
+    
+@app.route('/', methods=['GET', 'POST'])
 def receive_update():
-    if request.method == "POST":
+    if request.method == 'POST':
         print(request.json)
-        chat_id = request.json["message"]["chat"]["id"]
-        send_message(chat_id, "pong")
-    return {"ok": True}
+        chat_id = request.json['message']['chat']['id']
+        message_id = request.json['message']['message_id']
+        send_message(chat_id, message_id, 'pong')
+    return {'ok': True}
+
+
+if __name__ == "__main__":
+    app.run(port=8443)
