c077e93a77cc779e5685615c4a4c71da77bc016b
Александр Коробов <aleksandrkorobov@MacBook-Air-Aleksandr.local>
2023-12-20 07:33:32 +0700

    update add.py

--- app.py
+++ app.py
@@ -4,9 +4,17 @@ import os
 
 app = Flask(__name__)
 
+def create_webhook():
+    token = os.environ['TELEGRAM_TOKEN']
+    method = 'setWebhook'
+    web = 'https://flask-task-kambelloid.onrender.com/'
+    data = {'url': web}
+    return requests.get(f'https://api.telegram.org/bot{token}/{method}', data=data).content
+
 def send_message(chat_id, message_id, text):
     method = 'sendMessage'
     token = os.environ['TELEGRAM_TOKEN']
+    print(token)
     url = f'https://api.telegram.org/bot{token}/{method}'
     data = {'chat_id': chat_id, 'reply_to_message_id': message_id, 'text': text}
     requests.post(url, data=data)
@@ -22,4 +30,5 @@ def receive_update():
 
 
 if __name__ == "__main__":
-    app.run(port=os.environ['PORT'])
+    print(create_webhook())
+    app.run()
